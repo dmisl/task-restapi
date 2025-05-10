d67efab59f1afd14178a36d86365672e960e17cb
unknown <rithlesscorp@gmail.com>
2025-05-10 13:31:13 +0200

    chore(users_table): update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->string('name', 60);
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->unsignedBigInteger('position_id');
+            $table->string('photo');
+
+            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
