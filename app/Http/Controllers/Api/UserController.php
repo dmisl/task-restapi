dc8803f2f8ec68a70388cb27a18d100442e04a34
unknown <rithlesscorp@gmail.com>
2025-05-10 15:07:36 +0200

    feat(UserIndexRequest): add simple UserIndexRequest

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserIndexRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(UserIndexRequest $request)
     {
         return response()->json([
             'data' => 'hello'
---
+++ app/Http/Requests/UserIndexRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserIndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'page' => ['nullable', 'integer', 'min:1'],
+            'count' => ['nullable', 'integer', 'min:1']
+        ];
+    }
+}
