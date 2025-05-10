2e5235c08d75164ccfa8dcb468eeea73262ac51b
unknown <rithlesscorp@gmail.com>
2025-05-10 16:08:45 +0200

    feat(UserResource): add UserResource

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,8 +11,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    protected UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function index(UserIndexRequest $request)
     {
+        $users = $this->userService->getPaginatedUsers($request->count ?? 5, $request->page ?? 1);
+
         return response()->json(new ApiResource([
             'success' => 'true'
         ]));
--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -3,11 +3,12 @@
 namespace App\Service;
 
 use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserService
 {
-    public function getPaginatedUsers(int $count, int $page)
+    public function getPaginatedUsers(int $count, int $page): LengthAwarePaginator
     {
         return User::with('position')->orderBy('id', 'asc')->paginate($count, ['*'], 'page', $page);
-    }   
+    }
 }
