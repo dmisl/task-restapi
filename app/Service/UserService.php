1e5d7a8705883ab04251e8b37286c8091a8529af
unknown <rithlesscorp@gmail.com>
2025-05-11 15:46:18 +0200

    feat(UserService): add paginator validation (404) in getPaginatedUsers method

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -3,12 +3,25 @@
 namespace App\Service;
 
 use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserService
 {
     public function getPaginatedUsers(int $count, int $page): LengthAwarePaginator
     {
-        return User::with('position')->orderBy('id', 'asc')->paginate($count, ['*'], 'page', $page);
+        $paginator = User::with('position')->orderBy('id', 'asc')->paginate($count, ['*'], 'page', $page);
+        
+        if($paginator->isEmpty())
+        {
+            throw new HttpResponseException(
+                response()->json([
+                    'success' => false,
+                    'message' => 'Page not found',
+                ], 404)
+            );
+        }
+
+        return $paginator;
     }
 }
