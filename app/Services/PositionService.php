26ddb1a4be7aa34dfe510c786e5a824377e3e238
unknown <rithlesscorp@gmail.com>
2025-05-12 01:32:08 +0200

    feat(position index): position validation (404 code)

--- app/Services/PositionService.php
+++ app/Services/PositionService.php
@@ -3,11 +3,24 @@
 namespace App\Services;
 
 use App\Models\Position;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class PositionService
 {
     public function getAll()
     {
-        return Position::all();
+        $positions = Position::all();
+
+        if(!$positions)
+        {
+            throw new HttpResponseException(
+                response()->json([
+                    'success' => false,
+                    'message' => 'Positions not found',
+                ], 404)
+            );
+        }
+
+        return $positions;
     }
 }
