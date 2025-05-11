96e4f4b85c7b9fb6f506a81efa1d347b83658e59
unknown <rithlesscorp@gmail.com>
2025-05-11 15:42:36 +0200

    feat(UserIndexRequest): add failedValidation method (422) to user index route

--- app/Http/Requests/UserIndexRequest.php
+++ app/Http/Requests/UserIndexRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserIndexRequest extends FormRequest
 {
@@ -17,5 +19,16 @@ class UserIndexRequest extends FormRequest
             'page' => ['nullable', 'integer', 'min:1'],
             'count' => ['nullable', 'integer', 'min:1']
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'fails' => $validator->errors(),
+            ], 422)
+        );
     }
 }
