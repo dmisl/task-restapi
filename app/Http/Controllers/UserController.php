df74ee7680d80a7cebb68b0fc5563f4096da2326
unknown <rithlesscorp@gmail.com>
2025-05-12 20:05:28 +0200

    feat(UserController): add index method to UserController & getAll() method to UserService

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    protected UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index()
+    {
+        $users = $this->userService->get
+        return view('user.index');
+    }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,6 +8,11 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserService
 {
+    public function getAll()
+    {
+        return User::all();
+    }
+
     public function getPaginatedUsers(int $count, int $page): LengthAwarePaginator
     {
         $paginator = User::with('position')->orderBy('id', 'asc')->paginate($count, ['*'], 'page', $page);
